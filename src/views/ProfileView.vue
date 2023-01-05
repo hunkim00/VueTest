0ed6b9884f7af0e93c9441b21abc8580f2c93dd7
DESKTOP-4UK2MNJ\ESTSW-EQPMNT-NB-025 <kimhin1027@naver.com>
2023-01-05 14:11:45 +0900

    ESLint 설정

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="profile">
+        Hello! Welcome To MyProfile
+        <table>
+            <tr>
+                <th>
+                    이름
+                </th>
+                <th>
+                    직업
+                </th>
+                <th>
+                    성별
+                </th>
+                <th>
+                    이메일 주소
+                </th>
+            </tr>
+            <tr>
+                <td>김훈</td>
+                <td>개발자</td>
+                <td>남자</td>
+                <td>kimhun1027@naver.com</td>
+            </tr>
+        </table>
+    </div>
+</template>
